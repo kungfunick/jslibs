372351fe2c54a7334fd69ad15ba557881e3beb20
kungfunick <kungfunick.nh@googlemail.com>
2018-08-10 12:04:30 +0100

    added doc block to autosize js

--- src/autoresize/autoresize.js
+++ src/autoresize/autoresize.js
@@ -1,7 +1,11 @@
+/**
+ * @description Auto resizes textarea html element based on size of content
+ * @event       listens to window on page load
+ */
 window.addEventListener("load", function() {
-    //Don't use keypress event. keypress event doesn't detect backspace and delete keys. 
+    // Don't use keypress event. keypress event doesn't detect backspace and delete keys. 
     window.document.querySelector("textarea").addEventListener("keydown", function() {
-        var content = window.document.querySelector("textarea").value;
+        const content = window.document.querySelector("textarea").value;
         window.document.querySelector("#divContainer").innerHTML = content;
         window.document.querySelector("textarea").style.height = window.document.querySelector("#divContainer").scrollHeight + "px";
     }, false);
